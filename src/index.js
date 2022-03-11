11bb3d3675e04ba9c1eef650e7936224ad2480f1
Nicole <nicochen@pdx.edu>
2022-03-10 21:01:42 -0800

    Added form group submission

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// type writer effect for heading
 let i = 0;
 let headingText = "hello...!"; 
 let speed = 150;
@@ -11,3 +12,39 @@ function typeWriter() {
 }
 
 typeWriter();
+
+// to log form information submitted by user
+let form = document.querySelector('form');
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  let user = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let message = document.getElementById("message").value;
+
+  if (!name && !email && !message)
+    console.warn("You must enter some data to submit this form");
+  else {
+    console.group("======== Form Submission ========");
+
+    if (!user)
+      console.log("Name: no submission");
+    else
+      console.log("Name: " + user);
+
+    if (!email)
+      console.log("Email: no submission");
+    else
+      console.log("Email: " + email);
+
+    if (!message)
+      console.log("Message: no submission");
+    else
+      console.log("Message: " + message);
+
+    console.groupEnd();
+  }
+}
